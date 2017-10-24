cb420ec371ee18c91a91ee3c7adc5e311e5fec2a
tjvictor <guoyongcheng_61@163.com>
2017-10-24 17:02:10 +0800

    Add files via upload

---
+++ WebForm2.aspx
@@ -0,0 +1,119 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="AjaxWebSite.WebForm2" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+        <script src="Scripts/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="Scripts/common.js" type="text/javascript"></script>
+    <script src="Scripts/ajaxfileupload.js" type="text/javascript"></script>
+    <script type="text/javascript">
+
+        $(function () {
+            callAjax('webHandler.ashx?action=filelist', '', 'filelistCallback', '', '', '', '')
+        });
+
+        function filelistCallback(dataStr) {
+            var data = JSON.parse(dataStr)
+            if (data.Status == "ok") {
+                var fileList = data.callBackEntity;
+                for (var i = 0; i < data.CallBackEntity.length; i++ ) {
+                    var file = data.CallBackEntity[i];
+
+                    var filePathFormat = file.FilePath.replace(/\\/g, "/");
+                    var tr_pre = '<tr uid="' + filePathFormat + '"><td>';
+                    var fileNameLink = '<a href="javascript:void(0)" style="float:left;" onclick="downloading(' + "'" + file.FileName + "','" + filePathFormat + "'" + ')">' + file.FileName + '</a>';
+                    var fileDeleteLink = '<a href="javascript:void(0)" style="float:right;margin-left:20px;" onclick="deleteFile(' + "'" + filePathFormat + "'" + ')">Delete</a>';
+                    var fileDateSpan = '<span style="float:right">'+file.FileDate+'</span>';
+                    var tr_suf = '</td></tr>';
+
+                    $('#fileListTB').html($('#fileListTB').html() + tr_pre + fileNameLink + fileDeleteLink + fileDateSpan + tr_suf);
+                }
+            }
+        }
+
+        function uploading() {
+            $.ajaxFileUpload({
+                url: 'webHandler.ashx?action=uploadX', //文件上传到哪个地址，告诉ajaxFileUpload
+                secureuri: false, //一般设置为false
+                fileElementId: 'fileUpload', //文件上传控件的Id  <input type="file" id="fileUpload" name="file" />
+                //dataType: 'json', //返回值类型 一般设置为json
+                success: function (dataObj, status)  //服务器成功响应处理函数
+                {
+                    var data = JSON.parse(dataObj.body.innerText)
+                    if (data.Status == "ok") {
+                        var file = data.CallBackEntity;
+                        $('#reminder').css("color", "green");
+                        $('#reminder').text(file.FileName);
+
+                        var filePathFormat = file.FilePath.replace(/\\/g, "/");
+                        var tr_pre = '<tr uid="' + filePathFormat + '"><td>';
+                        var fileNameLink = '<a href="javascript:void(0)" style="float:left;" onclick="downloading(' + "'" + file.FileName + "','" + filePathFormat + "'" + ')">' + file.FileName + '</a>';
+                        var fileDeleteLink = '<a href="javascript:void(0)" style="float:right;margin-left:20px;" onclick="deleteFile(' + "'" + filePathFormat + "'" + ')">Delete</a>';
+                        var fileDateSpan = '<span style="float:right">' + file.FileDate + '</span>';
+                        var tr_suf = '</td></tr>';
+
+                        $('#fileListTB').html(tr_pre + fileNameLink + fileDeleteLink + fileDateSpan + tr_suf + $('#fileListTB').html());
+
+                    } else {
+                        $('#reminder').css("color", "red");
+                        $('#reminder').text(data.Prompt);
+                    }
+                },
+                error: function (xhr, textStatus) {
+
+                },
+                complete: function (data) {
+
+                }
+            });
+        }
+
+        function deleteFile(filePath) {
+            callAjax('webHandler.ashx?action=delete&filePath=' + filePath, '', 'deleteCallback', '', '', '', '')
+        }
+
+        function deleteCallback(dataStr) {
+            var data = JSON.parse(dataStr)
+            if (data.Status == "ok") {
+                $('#reminder').css("color", "green");
+                $('#reminder').text(data.Prompt);
+
+                $('#fileListTB tr[uid="' + data.CallBackEntity.FilePath + '"]').remove();
+
+            } else {
+                $('#reminder').css("color", "red");
+                $('#reminder').text(data.Prompt);
+            }
+        }
+
+        function downloading(fileName, filePath) {
+            var form = $("<form>"); //定义一个form表单
+            form.attr("style", "display:none");
+            form.attr("target", "");
+            form.attr("method", "post");
+            form.attr("action", "webHandler.ashx?action=download&fileName=" + fileName + "&filePath=" + filePath);
+            var input1 = $("<input>");
+            input1.attr("type", "hidden");
+            input1.attr("name", "webHandler.ashx?action=download");
+            input1.attr("value", (new Date()).getMilliseconds());
+            $("body").append(form); //将表单放置在web中
+            form.append(input1);
+
+            form.submit(); //表单提交 
+        }
+    </script>
+</head>
+<body>
+    <div>
+        <input id="fileUpload" type="file" name="file1" onchange="uploading();"/>
+        <span id="reminder"></span>
+    </div>
+    <div style="width:100%;height:1px;background-color:blue;margin:10px 0 10px 0;"></div>
+    <div>
+        <table id="fileListTB" style="width:100%">
+        </table>
+    </div>
+</body>
+</html>
---
+++ webHandler.ashx.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace AjaxWebSite
+{
+    /// <summary>
+    /// Summary description for webHandler
+    /// </summary>
+    public class webHandler : IHttpHandler
+    {
+        private class FileBrief
+        {
+            private string fileName;
+
+            public string FileName
+            {
+                get { return fileName; }
+                set { fileName = value; }
+            }
+            private string fileDate;
+
+            public string FileDate
+            {
+                get { return fileDate; }
+                set { fileDate = value; }
+            }
+            private string filePath;
+
+            public string FilePath
+            {
+                get { return filePath; }
+                set { filePath = value; }
+            }
+        }
+
+        private class ResponseEntity
+        {
+            private string status;
+
+            public string Status
+            {
+                get { return status; }
+                set { status = value; }
+            }
+            private string prompt;
+
+            public string Prompt
+            {
+                get { return prompt; }
+                set { prompt = value; }
+            }
+            private object callBackEntity;
+
+            public object CallBackEntity
+            {
+                get { return callBackEntity; }
+                set { callBackEntity = value; }
+            }
+
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            switch (context.Request.QueryString["action"])
+            {
+                case "uploadX": UploadAnyFile(context); break;
+                case "upload": UploadFile(context); break;
+                case "download": DownloadFile(context); break;
+                case "filelist": fileList(context); break;
+                case "delete": DeleteFile(context); break;
+                default: break;
+            }
+        }
+
+        private void DeleteFile(HttpContext context)
+        {
+            string path = context.Request.QueryString["filePath"];
+
+            FileBrief fb = new FileBrief();
+            fb.FilePath = path;
+
+            File.Delete(path);
+
+            ResponseEntity re = new ResponseEntity();
+            re.Status = "ok";
+            re.Prompt = "ok";
+            re.CallBackEntity = fb;
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            context.Response.Write(serializer.Serialize(re));
+
+        }
+
+        private void fileList(HttpContext context)
+        {
+            DirectoryInfo folder = new DirectoryInfo(@"C:\temp\classes");
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            List<FileBrief> fileBriefList = new List<FileBrief>();
+
+            foreach (FileInfo file in folder.GetFiles("*"))
+            {
+                FileBrief fb = new FileBrief();
+                fb.FileName = file.Name;
+                fb.FilePath = file.FullName;
+                fb.FileDate = file.LastWriteTime.ToString("yyyy-MM-dd");
+
+                fileBriefList.Add(fb);
+            }
+
+            ResponseEntity re = new ResponseEntity();
+            re.Status = "ok";
+            re.Prompt = "ok";
+            re.CallBackEntity = fileBriefList;
+
+            context.Response.Write(serializer.Serialize(re));
+        }
+
+        private void UploadFile(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            HttpServerUtility server = context.Server;
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            HttpPostedFile file = context.Request.Files[0];
+            if (file.ContentLength > 0)
+            {
+                string extName = Path.GetExtension(file.FileName);
+                string fileName = Guid.NewGuid().ToString();
+                string fullName = fileName + extName;
+
+                string imageFilter = ".jpg|.png|.gif|.ico";// 随便模拟几个图片类型
+                if (imageFilter.Contains(extName.ToLower()))
+                {
+                    string phyFilePath = @"C:\uploadImg\" + fullName;
+                    file.SaveAs(phyFilePath);
+                    response.Write("上传成功！文件名：" + fullName + "<br />");
+                    response.Write(string.Format("<img src='Upload/Image/{0}'/>", fullName));
+                }
+            }
+        }
+
+        private void UploadAnyFile(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            HttpServerUtility server = context.Server;
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            HttpPostedFile file = context.Request.Files[0];
+            if (file.ContentLength > 0)
+            {
+                string phyFilePath = @"C:\temp\classes\" + file.FileName;
+                file.SaveAs(phyFilePath);
+
+                ResponseEntity re = new ResponseEntity();
+                re.Status = "ok";
+                re.Prompt = "ok";
+                FileInfo refile = new FileInfo(phyFilePath);
+                FileBrief fb = new FileBrief();
+                fb.FileName = refile.Name;
+                fb.FileDate = refile.LastWriteTime.ToString("yyyy-MM-dd");
+                fb.FilePath = refile.FullName;
+                re.CallBackEntity = fb;
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                string str = serializer.Serialize(re);
+                response.Write(str);
+            }
+        }
+
+        private void DownloadFile(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            HttpServerUtility server = context.Server;
+            HttpRequest request = context.Request;
+            HttpResponse response = context.Response;
+
+            string filePath = context.Request.QueryString["filePath"];
+            string fileName = context.Request.QueryString["fileName"];
+            
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+            byte[] bytes = new byte[(int)fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+            fs.Close();
+            response.ContentType = "application/octet-stream";
+            //通知浏览器下载文件而不是打开
+            response.AddHeader("Content-Disposition", "attachment; filename=" + HttpUtility.UrlEncode(fileName, System.Text.Encoding.UTF8));
+            response.BinaryWrite(bytes);
+            response.Flush();
+            response.End();
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
